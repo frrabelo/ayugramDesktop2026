27fe9dc4e557f25c4b2b6f1ea348471fb90ee7eb
Francisco Rabelo <frrabelo@gmail.com>
2026-06-06 01:51:16 -0300

    fix(ci): resolve erros de link de brotli e hime e reverte para imagem docker oficial

--- Telegram/SourceFiles/ayu/ayu_infra.cpp
+++ Telegram/SourceFiles/ayu/ayu_infra.cpp
@@ -77,4 +77,22 @@ void init() {
 	initTranslator();
 }
 
+} // namespace AyuInfra
+
+#if defined(__linux__) || defined(__FreeBSD__)
+#include <dlfcn.h>
+#include <memory>
+
+namespace base {
+    template <auto Method>
+    struct custom_delete;
 }
+
+namespace base {
+namespace Platform {
+    void *LoadSymbolGeneric(std::unique_ptr<void, base::custom_delete<&dlclose>> const &handle, const char *name) {
+        return dlsym(handle.get(), name);
+    }
+}
+}
+#endif
